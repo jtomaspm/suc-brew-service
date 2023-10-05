1645ce943b285f1909ab20a086bd15c1e0327709
João Machado <jtomaspm@ua.pt>
2023-10-05 22:29:16 +0100

    BATMAN

---
+++ src/test/java/guru/springframework/sucbrewservice/SucBrewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sucbrewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucBrewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
